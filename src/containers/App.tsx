af93ee2cb27f15de70e491840eda379e85442c07
Zhuravlev Petr <zeterfrost@gmail.com>
2023-11-17 10:37:40 +0600

    added function for update message in App container

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -4,38 +4,59 @@ import {MessageFormType, MessageType} from '../types.d..ts';
 import MemoSpinner from '../components/Spinner/Spinner.tsx';
 import MemoSendForm from '../components/SendForm/SendForm.tsx';
 
-const url = 'http://146.185.154.90:8000/messages';
+const baseUrl = (time: string = ''): string => `http://146.185.154.90:8000/messages${time}`;
+let lastMessageTime: string = '';
 
 const App = () => {
   const [messages, setMessages] = useState<MessageType[]>([]);
   const [showSpinner, setShowSpinner] = useState<boolean>(false);
 
   useEffect(() => {
-    const getData = async () => {
+
+    const getData = async (url: string) => {
       try {
-        setShowSpinner(true);
         const response: Response = await fetch(url);
         if (response.ok) {
-          const messages: MessageType[] = await response.json();
-          setMessages(messages);
+          const data: MessageType[] = await response.json();
+          if (data.length !== 0) {
+            lastMessageTime = `?datetime=${data[data.length - 1].datetime}`;
+            setMessages(prevState => {
+              let allMessages: MessageType[];
+              if (url === baseUrl()) {
+                allMessages = data;
+              } else {
+                allMessages = [...prevState, ...data];
+              }
+              return allMessages;
+            });
+          }
         }
-        setShowSpinner(false);
       } catch (e: Error) {
-        alert(e);
+        console.log('===========================');
+        console.log(e.message);
+        console.log(lastMessageTime);
+        console.log(url);
       }
-
     };
 
-    void getData();
+    const run = async () => {
+      setShowSpinner(true);
+      void await getData(baseUrl());
+      setShowSpinner(false);
+      setInterval(() => {
+        void getData(baseUrl(lastMessageTime));
+      }, 3000);
+    };
+
+    void run();
   }, []);
 
   const onSubmit = async (message: MessageFormType) => {
-    console.log(message);
     const data: URLSearchParams = new URLSearchParams();
     data.set('message', message.message);
     data.set('author', message.auhtor);
 
-    const response: Response = await fetch(url, {
+    const response: Response = await fetch(baseUrl(), {
       method: 'post',
       body: data,
     });

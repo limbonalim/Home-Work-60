d2db2a3ec4bed8a890090bea43919bc3f2d31efd
Zhuravlev Petr <zeterfrost@gmail.com>
2023-11-17 12:55:02 +0600

    fix bug in App container

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -7,44 +7,18 @@ import FormatDate from '../components/FormatDate/FormatDate.ts';
 
 const baseUrl = (time: string = ''): string => `http://146.185.154.90:8000/messages${time}`;
 let lastMessageTime: string = '';
+let interval: number;
 
 const App = () => {
   const [messages, setMessages] = useState<MessageType[]>([]);
   const [showSpinner, setShowSpinner] = useState<boolean>(false);
 
   useEffect(() => {
-
-    const getData = async (url: string) => {
-      try {
-        const response: Response = await fetch(url);
-        if (response.ok) {
-          const data: MessageType[] = await response.json();
-          if (data.length !== 0) {
-            lastMessageTime = `?datetime=${data[data.length - 1].datetime}`;
-            setMessages(prevState => {
-              let allMessages: MessageType[];
-              if (url === baseUrl()) {
-                allMessages = data;
-              } else {
-                allMessages = [...prevState, ...data];
-              }
-              return allMessages;
-            });
-          }
-        }
-      } catch (e: Error) {
-        console.log('===========================');
-        console.log(e.message);
-        console.log(lastMessageTime);
-        console.log(url);
-      }
-    };
-
     const run = async () => {
       setShowSpinner(true);
       void await getData(baseUrl());
       setShowSpinner(false);
-      setInterval(() => {
+      interval = setInterval(() => {
         void getData(baseUrl(lastMessageTime));
       }, 3000);
     };
@@ -52,7 +26,31 @@ const App = () => {
     void run();
   }, []);
 
+  const getData = async (url: string) => {
+    try {
+      const response: Response = await fetch(url);
+      if (response.ok) {
+        const data: MessageType[] = await response.json();
+        if (data.length !== 0) {
+          lastMessageTime = `?datetime=${data[data.length - 1].datetime}`;
+          setMessages(prevState => {
+            let allMessages: MessageType[];
+            if (url === baseUrl()) {
+              allMessages = data;
+            } else {
+              allMessages = [...prevState, ...data];
+            }
+            return allMessages;
+          });
+        }
+      }
+    } catch (e: Error) {
+      alert(e.message);
+    }
+  };
+
   const onSubmit = async (message: MessageFormType) => {
+    clearInterval(interval);
     const data: URLSearchParams = new URLSearchParams();
     data.set('message', message.message);
     data.set('author', message.auhtor);
@@ -61,6 +59,10 @@ const App = () => {
       method: 'post',
       body: data,
     });
+    interval = setInterval(() => {
+      void getData(baseUrl(lastMessageTime));
+    }, 3000);
+
   };
 
   const listOfMessages: MemoMessage[] = messages.map((message) => {
